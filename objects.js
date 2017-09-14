5b527a958a47354ea7f5dcba35ff8d0e46c03ee3
Chris Cardle <cdcardle@gmail.com>
2017-09-14 22:13:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,7 @@ playlist[`Weird Al`] = `I'm fat`
 function updatePlaylist(obj, artist, song) {
   return Object.assign({}, obj, {[artist]: song})
 }
-function removeFromPlaylist(obj, artist) {
-  delete obj.artist;
-  return obj;
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist;
 }
