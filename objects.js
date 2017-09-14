2e6382bed76bb596e96e63fabc6e0fc363429803
Chris Cardle <cdcardle@gmail.com>
2017-09-14 22:13:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,11 @@
-//define playlist
+
 var playlist = {};
 playlist[`Weird Al`] = `I'm fat`
 
 function updatePlaylist(obj, artist, song) {
   return Object.assign({}, obj, {[artist]: song})
 }
+
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
   return playlist;
